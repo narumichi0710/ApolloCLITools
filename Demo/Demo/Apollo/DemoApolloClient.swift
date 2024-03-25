3dd3a8d2b5a3fd36e446fe3d6ba5dec28ab0d319
Narumichi Kubo <narumichi7373@gmail.com>
2024-03-25 23:19:01 +0900

    add apolloClient

---
+++ Demo/Demo/Apollo/DemoApolloClient.swift
@@ -0,0 +1,32 @@
+//
+//  DemoApolloClient.swift
+//  Demo
+//
+//  Created by Narumichi Kubo on 2024/03/25.
+//
+
+import Apollo
+import Foundation
+
+public struct DemoApolloClient {
+    public init() {}
+
+    public let client: ApolloClient = {
+        let client = URLSessionClient()
+        let cache = InMemoryNormalizedCache()
+        let store = ApolloStore(cache: cache)
+        let provider = NetworkInterceptorProvider(store: store, client: client)
+        let url = URL(string: "https://swapi-graphql.netlify.app/.netlify/functions/index/graphql")!
+        let transport = RequestChainNetworkTransport(
+            interceptorProvider: provider,
+            endpointURL: url,
+            useGETForQueries: true,
+            useGETForPersistedQueryRetry: true
+        )
+        return ApolloClient(networkTransport: transport, store: store)
+    }()
+}
+
+enum ApolloError: Error {
+    case fetal
+}
