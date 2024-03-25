7ee94070d17fa229ea70dbd53c1224b24d56d135
Narumichi Kubo <narumichi7373@gmail.com>
2024-03-25 23:06:33 +0900

    add generate code script

---
+++ Sources/GenerateCode/GenerateCode.swift
@@ -0,0 +1,46 @@
+//
+//  GenerateCode.swift
+//
+//
+//  Created by Narumichi Kubo on 2024/03/25.
+//
+
+import Apollo
+import ApolloCodegenLib
+import ArgumentParser
+import Foundation
+
+@main
+struct GenerateCode: AsyncParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "generate",
+        abstract: "Generates Swift code from the provided GraphQL schema"
+    )
+
+    mutating func run() async throws {
+        #if os(macOS)
+            let demoPath = "./Demo/Demo/"
+            let schemaFilePaths = [demoPath + "Apollo/Generated/Schema/schema.graphqls"]
+            let operationSearchPaths = ["./Demo/Demo/Operation/*.graphql"]
+            let schemaConfigPath = demoPath + "Apollo/Generated/Operation/"
+            let relativePath = "Generated/"
+
+            let configuration = ApolloCodegenConfiguration(
+                schemaNamespace: "GraphQL",
+                input: .init(
+                    schemaSearchPaths: schemaFilePaths,
+                    operationSearchPaths: operationSearchPaths
+                ),
+                output: .init(
+                    schemaTypes: ApolloCodegenConfiguration.SchemaTypesFileOutput(
+                        path: schemaConfigPath,
+                        moduleType: .embeddedInTarget(name: "Demo", accessModifier: .internal)
+                    ),
+                    operations: .relative(subpath: relativePath, accessModifier: .internal),
+                    testMocks: .none
+                )
+            )
+            try await ApolloCodegen.build(with: configuration)
+        #endif
+    }
+}
